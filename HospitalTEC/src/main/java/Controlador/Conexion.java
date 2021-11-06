9b25626da8c7891bab8858bb3f426ccc204f8a88
Pablo Chaves <pablo290418@gmail.com>
2021-11-06 17:42:20 -0600

    Inicio

---
+++ HospitalTEC/src/main/java/Controlador/Conexion.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ * Abstracción de la clase Conexion
+ * @author Pablo Chaves, Aaron Soto y Luis Leitón
+ * @version (06/11/2021)
+ */
+public class Conexion {
+  Connection conectar = null;
+  String usuario = "admin";
+  String contrasena = "sql123";
+  String cadenaBase = "jdbc:sqlserver://localhost:1433/HospitalTEC";
+  
+  /**
+   * Método constructor conexion
+   */
+  public Conexion(){
+  }
+  
+  /**
+   * Método para obtener la conexión con la base de datos
+   * @return el driver con la conexión a la base de datos
+   */
+  public Connection getConexion(){
+    String conexionUrl = "jdbc:sqlserver://localhost:1433;databaseName=HospitalTEC";
+    
+    try{
+      conectar = DriverManager.getConnection(conexionUrl, usuario, contrasena);
+      return conectar;
+    }catch(SQLException e){
+      System.out.println("Error detectado.");
+      System.out.println(e.toString());
+      return null;
+    }
+  }
+}
---
+++ HospitalTEC/src/main/java/Modelo/Persona.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ * Abstraccion de la clase persona
+ * @author Pablo Chaves, Aaron Soto y Luis Leitón
+ * @version (06/11/2021)
+ */
+public abstract class Persona {
+  protected int id;
+  protected String nombre;
+  protected String apellido1;
+  protected String apellido2;
+
+  /**
+   * 
+   * @param id
+   * @param nombre 
+   */
+  public Persona(int pId, String pNombre) {
+    this.id = pId;
+    this.nombre = pNombre;
+  }
+
+  /**
+   * 
+   * @return 
+   */
+  public int getId() {
+    return id;
+  }
+
+  /**
+   * 
+   * @param id 
+   */
+  public void setId(int pId) {
+    this.id = pId;
+  }
+
+  /**
+   * 
+   * @return 
+   */
+  public String getNombre() {
+    return nombre;
+  }
+
+  /**
+   * 
+   * @param nombre 
+   */
+  public void setNombre(String pNombre) {
+    this.nombre = pNombre;
+  }
+
+  /**
+   * 
+   * @return 
+   */
+  public String getApellido1() {
+    return apellido1;
+  }
+
+  /**
+   * 
+   * @param apellido1 
+   */
+  public void setApellido1(String pApellido1) {
+    this.apellido1 = pApellido1;
+  }
+
+  /**
+   * 
+   * @return 
+   */
+  public String getApellido2() {
+    return apellido2;
+  }
+
+  /**
+   * 
+   * @param apellido2 
+   */
+  public void setApellido2(String pApellido2) {
+    this.apellido2 = pApellido2;
+  }
+  
+  
+}
