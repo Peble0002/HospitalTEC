c9830b6e04adcf25005b38ad5bd414898fb426a0
AaronSotco <65744689+AaronSotco@users.noreply.github.com>
2021-11-24 18:47:54 -0600

    Modificacion Diagnostico Asociado

--- HospitalTEC/src/main/java/Controlador/ControladorDiagnosticoPaciente.java
+++ HospitalTEC/src/main/java/Controlador/ControladorDiagnosticoPaciente.java
@@ -5,10 +5,15 @@
  */
 package Controlador;
 
+import DAO.Paciente_CitaBD;
 import Vista.DiagnosticosAsociadosPaciente;
 import Vista.VistaPaciente;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -31,7 +36,13 @@ public class ControladorDiagnosticoPaciente  implements ActionListener{
   public void actionPerformed(ActionEvent e){
     switch(e.getActionCommand()){
       case "Buscar":
-         //FUNCION
+          try{
+            cargarTabla();
+            break;
+          }catch (SQLException o){ 
+            JOptionPane.showMessageDialog(null, o.toString()); 
+            break;
+          }
       case "Volver":
           VistaPaciente VP = new VistaPaciente();
           ControladorVistaPaciente CVP = new ControladorVistaPaciente(VP,usuario);
@@ -43,5 +54,70 @@ public class ControladorDiagnosticoPaciente  implements ActionListener{
     }
   }
   
+  public String cargarQuery(String fechaI, String fechaF, String nivel, String nombreDiagnostico){
+    String consulta = "SELECT CatalogoDiagnosticos.IdDiagnostico, "
+            + "CatalogoDiagnosticos.NombreDiagnostico FROM Cita INNER JOIN "
+            + "Paciente_Cita ON Cita.IdCita = Paciente_Cita.IdCita INNER JOIN "
+            + "Cita_Diagnostico ON Cita.IdCita = Cita_Diagnostico.IdCita INNER "
+            + "JOIN CatalogoDiagnosticos ON Cita_Diagnostico.IdDiagnostico = "
+            + "CatalogoDiagnosticos.IdDiagnostico WHERE idPaciente = '" + 
+            usuario + "'";
+    if(fechaI != null){
+      consulta += " AND fecha >= '" + fechaI + "'";
+    }if(fechaF != null){
+      consulta += " AND fecha <= '" + fechaF + "'";
+    }if(!nivel.equals("-")){
+      consulta += " AND Nivel = '" + nivel + "'";
+    }if(!nombreDiagnostico.equals("-")){
+      consulta += " AND NombreDiagnostico LIKE '%" + nombreDiagnostico + "%'";
+    }
+    return consulta;
+  }
   
+  private String validarFecha(String dia, String mes, String ano){
+    if(dia.equals("-") || mes.equals("-") || ano.equals("-")){
+      return null;
+    }else{
+      return ano + "-" + mes + "-" + dia;
+    }
+  }
+  
+  private void cargarTabla() throws SQLException{
+    String dia = (String) vistaDiagnosticosAsociadosPaciente.cbDiaInicio.getSelectedItem();
+    String mes = (String) vistaDiagnosticosAsociadosPaciente.cbMesInicio.getSelectedItem();
+    String ano = (String) vistaDiagnosticosAsociadosPaciente.cbAnoInicio.getSelectedItem();
+
+    String fechaI = validarFecha(dia, mes, ano);
+   
+    String diaF = (String) vistaDiagnosticosAsociadosPaciente.cbDiaFin.getSelectedItem();
+    String mesF = (String) vistaDiagnosticosAsociadosPaciente.cbMesFin.getSelectedItem();
+    String anoF = (String) vistaDiagnosticosAsociadosPaciente.cbAnoFin.getSelectedItem();
+
+    String fechaF = validarFecha(diaF, mesF, anoF);
+
+    String nivel = (String) vistaDiagnosticosAsociadosPaciente.cbNivel.getSelectedItem();
+    String nombreDiagnostico = (String) vistaDiagnosticosAsociadosPaciente.cbNombreDiagnosticos.getSelectedItem();
+    
+    String consulta = cargarQuery(fechaI, fechaF, nivel, nombreDiagnostico);    
+
+    DefaultTableModel modeloTabla = (DefaultTableModel) vistaDiagnosticosAsociadosPaciente.tablaDiagnosticos.getModel();
+    modeloTabla.setRowCount(0);
+    
+    Paciente_CitaBD pacienteBD = new Paciente_CitaBD ();
+    ResultSet rs = pacienteBD.consultarCitasPaciente(consulta);
+    int columnas = rs.getMetaData().getColumnCount();
+
+    try{
+      while(rs.next()){
+      Object[] fila = new Object[columnas];
+      for(int indice = 0; indice < columnas; indice++){
+        fila[indice] = rs.getObject(indice+1);
+      }
+      modeloTabla.addRow(fila);
+      }
+
+    }catch(SQLException e){
+      JOptionPane.showMessageDialog(null, e.toString());
+    }
+  }
 }
